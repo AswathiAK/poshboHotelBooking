6571e8362a34c86c2ebd2e2f7ee0ae811e6ed31f
Aswathi A K <aswathiak.3@gmail.com>
2023-07-09 15:32:20 +0530

    mongodb,userRoute and adminRoute and all created

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+mongoose.set('strictQuery',true);
+//mongoose.connect("mongodb://127.0.0.1:27017/PoshboHotelBooking");
+mongoose.connect(process.env.MONGODB_URL);
+
 const express = require("express");
+const cors = require("cors");
+const userRoute = require('./routes/userRoute');
+const adminRoute = require('./routes/adminRoute');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send("BackEnd is Running");
 });
+
+//Routes
+app.use('/', userRoute);
+app.use('/admin', adminRoute);
+
 app.listen(PORT, () => {
   console.log(`Server is Running on http://localhost:${PORT}`);
-})
+});
