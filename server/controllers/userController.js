c090e715a1e38c7ca3814ec12424b6ce0ebe1d32
Aswathi A K <aswathiak.3@gmail.com>
2023-07-16 12:32:37 +0530

    user registration function in backend completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,22 +2,39 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
 
+const passwordHashing = async (password) => {
+  try {
+    const bcryptSalt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, bcryptSalt);
+    return hashPassword;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
 const userRegister = async (req, res) => {
   const { name, mobile, email, password } = req.body;
+  const securePassword = await passwordHashing(password);
   try {
     if (name && mobile && email && password) {      
       const isUser = await User.find({$or:[{email:email},{mobile:mobile}]});
       if (isUser) {
         res.status(400).json({ message: "User is Already Exist" });
       } else {
-        
-
+        const userData = new User({
+          name,
+          mobile,
+          email,
+          password: securePassword
+        });
+        const user = await userData.save();
+        res.status(201).json({ message: "Registered Successfully", user });
       }
     } else {
       res.status(400).json({ message: "All Fields are Required" });
     } 
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(422).json({ message: error.message });
   }
 };
 
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,5 +3,6 @@ const user_route = express.Router();
 const userController = require('../controllers/userController');
 
 user_route.post('/register', userController.userRegister);
+user_route.post('/otp', userController.enterOtp);
 
 module.exports = user_route;
